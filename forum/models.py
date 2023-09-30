c10d0593d425734cc5b18e4e0bdfc651618b38ed
AlexDralur <>
2023-09-30 23:29:50 +0000

    Add Comment model

--- forum/models.py
+++ forum/models.py
@@ -27,15 +27,33 @@ class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="posts_category")
+        User, on_delete=models.CASCADE, related_name="posts_list")
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
 
+    class Meta:
+        ordering = ['created_on']
+
     def __str__(self):
         return self.title
 
     def number_of_likes(self):
         return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.name}'
