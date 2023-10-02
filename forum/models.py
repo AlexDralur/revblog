effcac858dfab50ca8de2724225e117819215d0f
AlexDralur <>
2023-10-02 18:28:51 +0000

    Add relation between post and category

--- forum/models.py
+++ forum/models.py
@@ -33,6 +33,8 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='category')
 
     class Meta:
         ordering = ['created_on']
