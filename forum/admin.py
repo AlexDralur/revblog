4d755f51c4abadacfdbb579afc5d43a1ecee9c04
AlexDralur <>
2023-10-07 10:41:26 +0000

    Add list display and search attributes to admin panel

--- forum/admin.py
+++ forum/admin.py
@@ -5,7 +5,9 @@ from .models import Category, Post, Comment
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('title', 'created_on')
+    list_filter = ('created_on',)
+    list_display = ('title', 'slug', 'created_on')
+    search_fields = ('title',)
 
 
 admin.site.register(Post)
