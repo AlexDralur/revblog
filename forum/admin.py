c135e2c70251baff63581901474d71983f068d52
AlexDralur <>
2023-10-07 10:37:48 +0000

    Add filter to admin panel for categories

--- forum/admin.py
+++ forum/admin.py
@@ -5,6 +5,7 @@ from .models import Category, Post, Comment
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('title', 'created_on')
 
 
 admin.site.register(Post)
