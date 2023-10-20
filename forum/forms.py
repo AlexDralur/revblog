35908c86f2be64f02dcd89d13628b24c3709bdb2
AlexDralur <>
2023-10-20 23:05:17 +0000

    Create class PostForm in forms.py

--- forum/forms.py
+++ forum/forms.py
@@ -1,3 +1,7 @@
 from .models import Post, Comment
 from django import Django
 
+class PostForm(forms.Model):
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'featured_image',)
