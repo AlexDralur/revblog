b3ba858a66f7f96d5a9164976a80236928fe427b
AlexDralur <>
2023-10-30 12:05:01 +0000

    Add Crispy Forms to settings and create comment form

--- forum/forms.py
+++ forum/forms.py
@@ -5,3 +5,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'featured_image', 'category',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
